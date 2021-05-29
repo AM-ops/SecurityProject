4794593b56aa7bb9cc6423d5e274d188322d6ab1
Josh-SCG <joshua.esterhuizen27@yahoo.com>
2021-05-29 20:01:50 +0200

    Update algorithms.py

--- Algorithms/algorithms.py
+++ Algorithms/algorithms.py
@@ -2,8 +2,6 @@
 # J&A's Cryptography Project
 import math
 import random as r
-#from charset_normalizer import detect
-#from contextlib import suppress
 
 #Formatting files
 #for any type of file :D
@@ -19,16 +17,12 @@ def fileToByteString(file):
     f.close()
     return byteStream
 
-
 def byteStringToFile(byteStream,file):
     f = open(file, 'wb')
     byteStream = bytearray(byteStream)
     f.write(byteStream)
     f.close()
     return
-
-#d = fileToByteString("file.jpg")
-#byteStringToFile(d,"file2.jpg")
 
 #Transposition
 #String to Matrix function
@@ -64,6 +58,10 @@ def StrToMatrix_FILE(text,key):
             matrix.append(row)
             row = []
             count = 0
+        elif(length == 0):
+            while(len(row)<key):
+                row.append(ord("\u0000"))
+            matrix.append(row)
 
     return matrix
 
@@ -86,7 +84,41 @@ def Transposition_TEXT_Decryption(encryptedMessage, key):
 
     for item in encryptedMessage:
         decryptedMessage[col] += item
-        print(item)
+        col += 1
+        if (col == numberColumns) or (col == numberColumns - 1 and row >= numberRows - numberBlanks):
+            col = 0
+            row += 1
+
+    return ''.join(decryptedMessage)
+
+def Transposition_FILE_Encryption(message, key):
+
+    f = open("UN-encryptedData.txt", 'wb')
+    f.write( bytearray(message))
+    f.close()
+
+    matrix = StrToMatrix_FILE(message, key)
+    encryptedMessage = []
+    for j in range(0,key):
+        for item in matrix:
+            strList = item.pop(0)
+            encryptedMessage.append(strList)
+
+    f = open("encryptedData.txt", 'wb')
+    f.write( bytearray(encryptedMessage))
+    f.close()
+
+    return encryptedMessage
+
+def Transposition_FILE_Decryption(message,key):
+    numberRows = math.ceil(len(message) / key)
+    numberColumns = key
+    numberBlanks = (numberColumns * numberRows) - len(message)
+    decryptedMessage = ['']*numberColumns
+    col,row = 0,0
+
+    for item in message:
+        decryptedMessage[col] += str(item)
         col += 1
         if (col == numberColumns) or (col == numberColumns - 1 and row >= numberRows - numberBlanks):
             col = 0
@@ -95,76 +127,15 @@ def Transposition_TEXT_Decryption(encryptedMessage, key):
     return ''.join(decryptedMessage)
 
 '''
-test = "hello world"
-en = Transposition_TEXT_Encryption(test,4)
-print(en)
-de = Transposition_TEXT_Decryption(en,4)
-print(de)
-'''
-
-
-def Transposition_FILE_Encryption(message, key):
-    matrix = StrToMatrix_FILE(message, key)
-    encryptedMessage = []
-    for j in range(0,key):
-        for item in matrix:
-            strList = item.pop(0)
-            encryptedMessage.append(strList)
-
-    return encryptedMessage
-
-def Transposition_FILE_Decryption(message, key):
-    numberColumns = math.ceil(len(message) / key)
-    numberRows = key
-    numberBlanks = (numberColumns * numberRows) - len(message)
-    decryptedMatrix = []*numberColumns
-    bufferRow = []
-    col,row = 0,0
-
-    for item in message:
-        bufferRow.append(item)
-        col += 1
-        if (col == numberColumns) or (col == numberColumns - 1 and row >= numberRows - numberBlanks):
-            col = 0
-            row += 1
-            decryptedMatrix.append(bufferRow)
-            bufferRow = []
-
-    decryptedMessage = []
-    for j in range(0,key):
-        for item in decryptedMatrix:
-            strList = item.pop(0)
-            decryptedMessage.append(strList)
-
-    return decryptedMessage
-
-'''
-input = fileToByteString("file.jpg")
+input = fileToByteString("file.mp3")
 encrypt = Transposition_FILE_Encryption(input,20)
-byteStringToFile(encrypt,"file3.jpg")
-decrypt = Transposition_FILE_Decryption(encrypt,20)
-byteStringToFile(decrypt,"file2.jpg")
-print(len(input))
-print(len(encrypt))
-print(len(decrypt))
 
-for i in range(0,50):
-    print(input[i*20],"and",encrypt[i],"and",decrypt[i])
-
-
-print(len(input))
-print(len(encrypt))
-print(len(decrypt))
-count = 0
-for i in range(0,len(input)):
-    if(input[i] == decrypt[i]):
-        print(input[i],' ',decrypt[i],'@',i)
-        count+=1
-print(count)
+for i in range(0,x):
+    print(input[i*20],"and",encrypt[i])
 '''
 #Vignere
 #Set Up Vigenere Table
-#!!!!!!!!!!!!!!!!!!! Not required; left in for completions sake !!!!!!!!!!!!!!!!
+'''
 VignereTable = []*26
 alphabet  = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
 
@@ -177,7 +148,7 @@ for i in range(0,26):
         addRow.append(alphabet[k])
     VignereTable.append(addRow)
     addRow = []
-
+'''
 #Fixes key length to match size of message
 def extendKeyLength(messageLength,key):
     keyLength = len(key)
@@ -188,11 +159,11 @@ def extendKeyLength(messageLength,key):
     else:
         for i in range(0,(messageLength - keyLength)):
             newKey.append(key[i % keyLength])
+        print(len(newKey))
         return ("".join(newKey)).upper()
 
 #Text algorithms
 def Vigenere_TEXT_Encryption(message, key):
-    #message = message.replace(" ","")
     message = message.upper()
     messageLength = len(message)
     encryptedMessage = []
@@ -243,24 +214,6 @@ def Vigenere_FILE_Decryption(message, key):
 
     return decryptedMessage
 
-'''
-input = fileToByteString("file.mp3")
-encrypt = Vigenere_FILE_Encryption(input,"appayipyip")
-byteStringToFile(encrypt,"file3.mp3")
-decrypt = Vigenere_FILE_Decryption(encrypt,"appayipyip")
-byteStringToFile(decrypt,"file2.mp3")
-print(len(input))
-print(len(encrypt))
-print(len(decrypt))
-
-count = 0
-for i in range(0,len(input)):
-    if(input[i] != decrypt[i]):
-        print(input[i],' ',decrypt[i],'@',i)
-        count+=1
-print(count)
-'''
-
 #Vernam
 #Key generators
 def vernam_Key_Generator(messageLength):
@@ -308,13 +261,6 @@ def Vernam_TEXT_Decryption(message):
 
     return "".join(decryptedMessage)
 
-'''
-encrypt = Vernam_TEXT_Encryption("the yellow brown cow jumped over the laxadazical fox on new years ever every decade at exactly half past noon whilst the moon shines glow")
-print(encrypt)
-decrypt = Vernam_TEXT_Decryption(encrypt)
-print(decrypt)
-'''
-
 #File algorithms
 def Vernam_FILE_Encryption(message):
     messageLength = len(message)
@@ -351,24 +297,6 @@ def Vernam_FILE_Decryption(message):
         decryptedMessage.append(newDecrypted)
 
     return decryptedMessage
-
-'''
-input = fileToByteString("file.jpg")
-encrypt = Vernam_FILE_Encryption(input)
-byteStringToFile(encrypt,"encrypted.jpg")
-decrypt = Vernam_FILE_Decryption(encrypt)
-byteStringToFile(decrypt,"decrypted.jpg")
-print(len(input))
-print(len(encrypt))
-print(len(decrypt))
-
-count = 0
-for i in range(0,len(input)):
-    if(input[i] != decrypt[i]):
-        print(input[i],' ',decrypt[i],'@',i)
-        count+=1
-print(count)
-'''
 
 #Own algorithm
 def own_TEXT_Encryption(message, key):
@@ -414,21 +342,12 @@ def own_TEXT_Decryption(message, key):
 
     return ''.join(decryptedMessage)
 
-
-test = "the yellow brown cow jumped over the laxadazical fox on new years eve every decade at exactly half past noon whilst the moon shines glow"
-en = own_TEXT_Encryption(test,2000)
-print(en)
-de = own_TEXT_Decryption(en,2000)
-print(de)
-
-
 def own_FILE_Encryption(message, key):
     messageLength = len(message)
     numericEncryptedValues = []
     for item in message:
-        numeric = item
-        mathPart = (((key*numeric)) - key)
-        numericEncryptedValues.append(mathPart%256)
+        mathPart = (item - key)
+        numericEncryptedValues.append(mathPart)
 
     randomKey = []
     for i in range(0,messageLength):
@@ -458,31 +377,11 @@ def own_FILE_Decryption(message, key):
 
     partA = []
     for j in range(0,messageLength):
-        partA.append((encryptedMessage[j]-randomKey[j])%256)
+        partA.append((encryptedMessage[j]-randomKey[j]))
 
     decryptedMessage = []
     for item in partA:
-        numeric = item
-        unMathPart = ((numeric+key)/key)
-        decryptedMessage.append(int(unMathPart)%256)
+        unMathPart = (item+key)
+        decryptedMessage.append(unMathPart%256)
 
     return decryptedMessage
-
-
-'''
-input = fileToByteString("file.jpg")
-encrypt = own_FILE_Encryption(input,3)
-byteStringToFile(encrypt,"encrypted.jpg")
-decrypt = own_FILE_Decryption(encrypt,3)
-byteStringToFile(decrypt,"decrypted.jpg")
-print(len(input))
-print(len(encrypt))
-print(len(decrypt))
-
-count = 0
-for i in range(0,len(input)):
-    if(input[i] != decrypt[i]):
-        print(input[i],' ',decrypt[i],'@',i)
-        count+=1
-print(count)
-'''
